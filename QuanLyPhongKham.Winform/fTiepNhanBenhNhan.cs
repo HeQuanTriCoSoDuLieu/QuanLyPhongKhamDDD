149b600b6d3bb42efeb28274a9d6b8fc48f79916
dhdatyit.sgu@gmail.com <daty.danghuynh20497@gmail.com>
2017-10-31 16:22:13 +0700

    Thêm EF, dataprovider

---
+++ QuanLyPhongKham.Winform/fTiepNhanBenhNhan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyPhongKham.Winform
+{
+    public partial class fTiepNhanBenhNhan : Form
+    {
+        public fTiepNhanBenhNhan()
+        {
+            InitializeComponent();
+        }
+
+        private void fTiepNhanBenhNhan_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
