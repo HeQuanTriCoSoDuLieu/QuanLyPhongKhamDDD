4d9152d96e4e4da93e1eafd903672533adb5918f
DESKTOP-NCUMM56\HP <truongnguyenhoangthuong97@gmail.com>
2017-11-02 22:07:30 +0700

    thuong cập nhật DTO

---
+++ QuanLyPhongKham.Model/DTO/ChucDanh.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Model.DTO
+{
+    class ChucDanh
+    {
+        public int MACHUCDANH { get; set; }
+        public string TENCHUCDANH { get; set; }
+
+    }
+}
---
+++ QuanLyPhongKham.Model/DTO/NhaCungCap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Model.DTO
+{
+    class NhaCungCap
+    {
+        public int MANHACC { get; set; }
+        public string TENNHACC { get; set; }
+        public string DIACHI { get; set; }
+        public string SODT { get; set; }
+        public string EMAIL { get; set; }
+        public int MAQUOCGIA { get; set; }
+    }
+}
