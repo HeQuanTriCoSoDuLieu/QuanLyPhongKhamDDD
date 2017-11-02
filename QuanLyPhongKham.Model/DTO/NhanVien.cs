fe330310c53882e8fc6c56a9227a7cffa38d56b7
dhdatyit.sgu@gmail.com <daty.danghuynh20497@gmail.com>
2017-11-02 23:58:49 +0700

    commit

---
+++ QuanLyPhongKham.Model/DTO/NhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Model.DTO
+{
+    public class NhanVien
+    {
+        public int MaNV {get;set;}
+        public string HOTEN { get; set; }
+        public bool GIOITINH { get; set; }
+        public string SODT { get; set; }
+        public string EMAIL { get; set; }
+        public int MAKHOA { get; set; }
+        public int MACHUCDANH { get; set; }
+        public int MACHUCVU { get; set; }
+    }
+}
---
+++ QuanLyPhongKham.Model/DTO/PhieuNhap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Model.DTO
+{
+    class PhieuNhap
+    
+    {
+        public int MAPHIEUNHAP { get; set; }
+        public int MANV { get; set; }
+        public DateTime NGAYNHAP { get; set; }
+    }
+}
