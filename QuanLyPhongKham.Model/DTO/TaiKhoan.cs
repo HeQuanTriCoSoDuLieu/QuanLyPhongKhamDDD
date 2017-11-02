bc6f1a85cf9ccd0c3787919343ccf2ef7c019def
dhdatyit.sgu@gmail.com <daty.danghuynh20497@gmail.com>
2017-11-02 21:24:30 +0700

    thêm ví dụ

---
+++ QuanLyPhongKham.Model/DTO/TaiKhoan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhongKham.Model.DTO
+{
+    public class TaiKhoan
+    {
+        public int MATK { get; set; }
+
+        public string TENDANGNHAP { get; set; }
+
+        public string MATKHAU { get; set; }
+
+        public string TENHIENTHI { get; set; }
+
+        public int? MAPHANQUYEN { get; set; }
+
+        public bool? TRANGTHAI { get; set; }
+
+    }
+}
