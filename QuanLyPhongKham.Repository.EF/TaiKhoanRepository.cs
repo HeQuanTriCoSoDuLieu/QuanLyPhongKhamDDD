85a726d1fe0a2ee959830ab053d91b807a734755
Đặng Huỳnh Đạt Ý <dhdatyit.sgu@gmail.com>
2017-11-09 21:47:38 +0700

    Chạy thử chương trình

--- QuanLyPhongKham.Repository.EF/TaiKhoanRepository.cs
+++ QuanLyPhongKham.Repository.EF/TaiKhoanRepository.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿using QuanLyPhongKham.Model.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using QuanLyPhongKham.Model;
-using QuanLyPhongKham.Model.Interfaces;
+
 namespace QuanLyPhongKham.Repository.EF
 {
     public class TaiKhoanRepository : ITaiKhoanRepository
     {
         public bool Login(string userName, string passWord)
         {
-            return true;
+            return QuanLyPhongKhamDbContext.Instance.TAIKHOAN.Where(a => a.TenDangNhap == userName && a.MatKhau == passWord).Count() > 0;
         }
     }
 }
+
