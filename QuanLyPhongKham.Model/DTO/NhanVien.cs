1867689bfe2b18107a06d005202a39f3352cf4bf
DESKTOP-NCUMM56\HP <truongnguyenhoangthuong97@gmail.com>
2017-11-28 22:34:52 +0700

    thuong thêm DTO

--- QuanLyPhongKham.Model/DTO/NhanVien.cs
+++ QuanLyPhongKham.Model/DTO/NhanVien.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,33 @@ namespace QuanLyPhongKham.Model.DTO
 {
     public class NhanVien
     {
+        public NhanVien() { }
+
+        public NhanVien(int manv,string hoten,bool gioitinh,string sodt,string email, int makhoa, int machucdanh, int machucvu)
+        {
+            MaNV = manv;
+            HoTen = hoten;
+            GioiTinh = gioitinh;
+            SoDT = sodt;
+            Email = email;
+            MaKhoa = makhoa;
+            MaChucDanh = machucdanh;
+            MaChucVu = machucvu;
+        }
+        public NhanVien(DataRow row)
+        {
+            MaNV = (int)row["MANV"];
+            HoTen = row["HOTEN"].ToString();
+            GioiTinh = (bool)row["GIOITINH"];
+            SoDT = row["SODT"].ToString();
+            Email = row["EMAIL"].ToString();
+            MaKhoa = (int)row["MAKHOA"];
+            MaChucDanh = (int)row["MACHUCDANH"];
+            MaChucVu = (int)row["MACHUCVU"];
+        }
         public int MaNV {get;set;}
 
-        public string HoTeN { get; set; }
+        public string HoTen { get; set; }
 
         public bool GioiTinh { get; set; }
 
